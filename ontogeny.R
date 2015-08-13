dda2a147c685918a2426f598915e294188251626
James Bain <jamescbain@gmail.com>
2015-08-12 23:49:59 -0500

    reformat subset to be more readable

--- ontogeny.R
+++ ontogeny.R
@@ -12,8 +12,10 @@ growth<-growth[,-c(2,5,6,7,13,14,15,16,18,19,20,21,22,23,24,25,26,
                    47,48,49,50,51,52,53,54,55,56,57,58,59,68,69,70,
                    71,72,73,74,75,76,83)]
 growth<-select(growth,-(old_bro:sex_pre_sibN))
-colnames(growth)<-c("id","famid","year","sex","hand","birthdate","yob","colldate","age","dadid","momid",
-                    "cm","kgs","r2d","l2d","r3d","l3d","r4d","l4d","r5d","l5d","famsize","birth_interval")
+colnames(growth)<-c("id","famid","year","sex","hand","birthdate",
+                    "yob","colldate","age","dadid","momid","cm",
+                    "kgs","r2d","l2d","r3d","l3d","r4d","l4d",
+                    "r5d","l5d","famsize","birth_interval")
 growth<-within(growth,{
   r2d4d<-growth$r2d/growth$r4d
   l2d4d<-growth$l2d/growth$l4d
