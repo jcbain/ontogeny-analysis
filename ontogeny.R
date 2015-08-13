df4c026804c2a2846ed86c2395418ebcf94caf22
James Bain <jamescbain@gmail.com>
2015-08-12 23:46:48 -0500

    subset dataframe

--- ontogeny.R
+++ ontogeny.R
@@ -6,16 +6,21 @@ library(ggplot2)
 library(dplyr)
 
 #Clean up data
-growth<-growth[,-c(60:123)]
-growth<-growth[,-c(2,5,6,7,13,14,15,16,18,19,20,21,22,23,24,25,26,27,28,30,31,32,33,34,36,37,38,39,
-                   41,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,68,69,70,71,72,73,74,75,76,83)]
+growth<-select(growth,-(X2D_R1_P1:X5D_L4_P2))
+growth<-growth[,-c(2,5,6,7,13,14,15,16,18,19,20,21,22,23,24,25,26,
+                   27,28,30,31,32,33,34,36,37,38,39,41,43,44,45,46,
+                   47,48,49,50,51,52,53,54,55,56,57,58,59,68,69,70,
+                   71,72,73,74,75,76,83)]
+growth<-select(growth,-(old_bro:sex_pre_sibN))
 colnames(growth)<-c("id","famid","year","sex","hand","birthdate","yob","colldate","age","dadid","momid",
-                    "cm","kgs","r2d","l2d","r3d","l3d","r4d","l4d","r5d","l5d","old_bro","old_bro2",
-                    "old_bro3","old_sis","old_sis2","old_sis3","oldsib_order", "sex_pre_sib2",
-                    "sex_pre_sib","sex_pre_sibN","famsize","birth_interval")
-growth$r2d4d<-growth$r2d/growth$r4d
-growth$l2d4d<-growth$l2d/growth$l4d
-growth$cmsq<-growth$cm^2
+                    "cm","kgs","r2d","l2d","r3d","l3d","r4d","l4d","r5d","l5d","famsize","birth_interval")
+growth<-within(growth,{
+  r2d4d<-growth$r2d/growth$r4d
+  l2d4d<-growth$l2d/growth$l4d
+  cm2<-growth$cm^2
+  cm3<-growth$cm^3
+})
+
 
 plot(growth$age,growth$cm)
 plot(growth$age,growth$kgs)
