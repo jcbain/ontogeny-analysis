916f0a6490dcb56a8c57543b90e2833803fc3665
James Bain <jamescbain@gmail.com>
2015-08-13 23:37:49 -0500

    transform age variables

--- ontogeny.R
+++ ontogeny.R
@@ -6,12 +6,12 @@ library(ggplot2)
 library(dplyr)
 
 #Clean up data
-growth<-select(growth,-(X2D_R1_P1:X5D_L4_P2))
+growth<-select(growth,-(X2D_R1_P1:X5D_L4_P2)) # remove extra data
 growth<-growth[,-c(2,5,6,7,13,14,15,16,18,19,20,21,22,23,24,25,26,
                    27,28,30,31,32,33,34,36,37,38,39,41,43,44,45,46,
                    47,48,49,50,51,52,53,54,55,56,57,58,59,68,69,70,
                    71,72,73,74,75,76,83)]
-growth<-select(growth,-(old_bro:sex_pre_sibN))
+growth<-select(growth,-(old_bro:sex_pre_sibN)) # remove extra data
 colnames(growth)<-c("id","famid","year","sex","hand","birthdate",
                     "yob","colldate","age","dadid","momid","cm",
                     "kgs","r2d","l2d","r3d","l3d","r4d","l4d",
@@ -22,6 +22,8 @@ growth<-within(growth,{
   cm2<-growth$cm^2
   cm3<-growth$cm^3
   age_year<-round(growth$age)
+  age2<-growth$age^2
+  age3<-growth$age^3
 })
 
 # height by age year
